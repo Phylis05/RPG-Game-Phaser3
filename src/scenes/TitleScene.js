dc8fcb9267be9123ec3a0f4538c31f614b0a3064
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-07 18:07:55 +0300

    refactor code in title scene to ui button class  and player classes

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,5 +1,7 @@
+import Phaser from 'phaser';
+import UiButton from '../classes/UiButton';
+// import config from '../config/config';
 export default class TitleScene extends Phaser.Scene {
- 
   constructor() {
       super('Title');
   }
@@ -9,6 +11,16 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
-      
+    // create title text
+    this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Zenva MMORPG', { fontSize: '64px', fill: '#fff' });
+    this.titleText.setOrigin(0.5);
+
+    // create the Play game button
+    this.startGameButton = new UiButton(this, this.scale.width / 2, this.scale.height * 0.65, 'button1', 'button2', 'Start', this.startScene.bind(this, 'Game')); // NEW
+  }
+
+  // NEW function:
+  startScene(targetScene) { 
+    this.scene.start(targetScene);
   }
 }
