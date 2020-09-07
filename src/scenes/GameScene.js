5c64a8e19df664cdcc5d6e14b9fdb5f55e95e308
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-07 18:54:51 +0300

    launch two scences together and add coins increament method

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,33 +1,41 @@
 import Phaser from 'phaser';
 import Player from '../classes/Player';
-import Chest from '../classes/Chest';;
+import Chest from '../classes/Chest';
+// import Ui from 'UiScene';
 export default class GameScene extends Phaser.Scene {
  
   constructor() {
       super('Game');
   }
 
-  create() {
-    let goldPickupAudio = this.sound.add('goldSound');
-
-
-  
-    this.chest = new Chest(this, 300, 300, 'items', 0);
-  
-    this.wall = this.physics.add.image(500, 100, 'button1');
-    this.wall.setImmovable();
-  
-    this.player = new Player(this, 32, 32, 'characters', 0);
-  
-    this.physics.add.collider(this.player, this.wall);
-    this.physics.add.overlap(this.player, this.chest, function(player, chest) { goldPickupAudio.play(); chest.destroy(); });
-  
-    this.cursors = this.input.keyboard.createCursorKeys();
+  init() {
+    this.scene.launch('Ui');
   }
 
+  create() {
+    this.goldPickupAudio = this.sound.add('goldSound');  // NEW
+      
+    this.chest = new Chest(this, 300, 300, 'items', 0);
+      
+    this.wall = this.physics.add.image(500, 100, 'button1');
+    this.wall.setImmovable();
+      
+    this.player = new Player(this, 32, 32, 'characters', 0);
+      
+    this.physics.add.collider(this.player, this.wall);
+    this.physics.add.overlap(this.player, this.chest, this.collectChest, null, this); // NEW
+      
+    this.cursors = this.input.keyboard.createCursorKeys();
+}
 
   update() {
     this.player.update(this.cursors);
+  }
+
+  collectChest(player, chest) {
+    this.goldPickupAudio.play(); 
+    this.events.emit('updateScore', chest.coins);
+    chest.destroy(); 
   }
 
 }
--- src/scenes/UiScene.js
+++ src/scenes/UiScene.js
@@ -1,14 +1,26 @@
 export default class UiScene extends Phaser.Scene {
  
   constructor() {
-      super('Ui');
+    super('Ui');
   }
 
-  preload() {
-
+  init() {
+    this.gameScene = this.scene.get('Game');
   }
 
   create() {
-      
+    this.setupUiElements();
+    this.setupEvents();
+  }
+
+  setupUiElements() {
+    this.scoreText = this.add.text(35, 8, 'Coins: 0', {fontSize: '16px', fill: '#fff'}); // NEW
+    this.coinIcon = this.add.image(15, 15, 'items', 3);
+  }
+
+  setupEvents() {
+    this.gameScene.events.on('updateScore', (score) => {
+      this.scoreText.setText(`Coins: ${score}`);
+    });
   }
 }
