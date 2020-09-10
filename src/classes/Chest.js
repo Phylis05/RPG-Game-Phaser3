bc42e1442ac1eb24877dba1433cc90d049f5e8c9
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-10 19:48:17 +0300

    addcoins and id to chest constructor

--- src/classes/Chest.js
+++ src/classes/Chest.js
@@ -1,9 +1,10 @@
 import Phaser from 'phaser';
 export default class Chest extends Phaser.Physics.Arcade.Image {
-  constructor(scene, x, y, key, frame) {
+  constructor(scene, x, y, key, frame, coins, id) {
     super(scene, x, y, key, frame);
     this.scene = scene;
-    this.coins = 10;
+    this.coins = coins;
+    this.id = id;
     
     this.scene.physics.world.enable(this);
     this.scene.add.existing(this);
