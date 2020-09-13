5a6d212a0ef4fa5257c4afe572eb398705434a2e
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-13 11:36:22 +0300

    modify style.css path in index.html

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,9 +1,12 @@
+/* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 import Phaser from 'phaser';
 import items from '../../assets/images/items.png';
 import characters from '../../assets/images/characters.png';
 import goldSound from '../../assets/audio/Pickup.wav';
 import monsters from '../../assets/images/monsters.png';
+
+const jsonMap = require('../../assets/level/large_level.json');
 
 export default class BootScene extends Phaser.Scene {
   constructor() {
@@ -18,30 +21,30 @@ export default class BootScene extends Phaser.Scene {
   }
 
   loadImages() {
-    this.load.image('button1', 'assets/images/ui/blue_button01.png');
-    this.load.image('button2', 'assets/images/ui/blue_button02.png');
-    this.load.image('background', 'assets/level/background-extruded.png');
+    this.load.image('button1', require('../../assets/images/ui/blue_button01.png').default);
+    this.load.image('button2', require('../../assets/images/ui/blue_button02.png').default);
+    this.load.image('background', require('../../assets/level/background-extruded.png').default);
   }
 
   loadSpritesheets() {
-    this.load.spritesheet('items', '../../assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.spritesheet('characters', '../../assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.spritesheet('monsters', '../../assets/images/monsters.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('items', require('../../assets/images/items.png').default, { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('characters', require('../../assets/images/characters.png').default, { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('monsters', require('../../assets/images/monsters.png').default, { frameWidth: 32, frameHeight: 32 });
   }
 
   loadAudio() {
-    this.load.audio('goldSound', ['assets/audio/Pickup.wav']);
-    this.load.audio('enemyDeath', ['assets/audio/EnemyDeath.wav']);
-    this.load.audio('playerAttack', ['assets/audio/PlayerAttack.wav']);
-    this.load.audio('playerDamage', ['assets/audio/PlayerDamage.wav']);
-    this.load.audio('playerDeath', ['assets/audio/PlayerDeath.wav']);
+    this.load.audio('goldSound', require('../../assets/audio/Pickup.wav').default);
+    this.load.audio('enemyDeath', require('../../assets/audio/EnemyDeath.wav').default);
+    this.load.audio('playerAttack', require('../../assets/audio/PlayerAttack.wav').default);
+    this.load.audio('playerDamage', require('../../assets/audio/PlayerDamage.wav').default);
+    this.load.audio('playerDeath', require('../../assets/audio/PlayerDeath.wav').default);
   }
 
   loadTileMap() {
-    this.load.tilemapTiledJSON('map', 'assets/level/large_level.json');
+    this.load.tilemapTiledJSON('map', jsonMap);
   }
 
   create() {
-    this.scene.start('Title');
+    this.scene.start('GameOver');
   }
 }
